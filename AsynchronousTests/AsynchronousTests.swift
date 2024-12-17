7eda5ca1ce1e0cb85fe27e86253677b1df779b95
Javier Heisecke <javierheiseckee@gmail.com>
2024-12-17 10:07:17 -0300

    Add AsynchronousTests and TestPlan

---
+++ AsynchronousTests/AsynchronousTests.swift
@@ -0,0 +1,16 @@
+//
+//  AsynchronousTests.swift
+//  AsynchronousTests
+//
+//  Created by Javier Heisecke on 2024-12-17.
+//
+
+import Testing
+
+struct AsynchronousTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
